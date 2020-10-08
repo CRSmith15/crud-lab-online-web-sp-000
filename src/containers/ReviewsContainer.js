18474d3426d583acaf21de88c001abf6d8dd10c5
Chance Smith <chancesmith97@hotmail.com>
2020-10-08 23:22:23 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import ReviewInput from '../components/reviews/ReviewInput';
 import Reviews from '../components/reviews/Reviews';
+import { connect } from 'react-redux';
 
 class ReviewsContainer extends Component {
 
   render() {
+    const { restaurant, addReview, deleteReview, reviews } = this.props;
+    const restaurantReviews = reviews.filter(review => review.restaurantId === restaurant.id);
+
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput
+          restaurantId ={restaurant.id}
+          addReview={addReview}
+        />
+        <Reviews
+        reviews={restaurantReviews}
+        deleteReview={deleteReview}
+        />
       </div>
     )
   }
