d002dbaca99baf9e3760adfc4bd3f18e34278e0a
Chance Smith <chancesmith97@hotmail.com>
2020-10-08 23:17:55 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: '',
+    restaurantId: this.props.restaurantId
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addReview(this.state);
+    this.setState({ text: '' });
+  }
+
+  handleChange = event => {
+    this.setState({ text: event.target.value })
+  }
+
+
   render() {
     return (
       <div>
